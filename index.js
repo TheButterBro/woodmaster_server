d228c2d3277b02450ad5572fd23680666597b02c
Maxim <butterbro1@gmail.com>
2022-10-03 02:00:26 +0400

    Trying fix PORT problem

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json());
