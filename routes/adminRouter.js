7d5a23be919dc573bcea32bdbe951baf27148c0e
Maxim <butterbro1@gmail.com>
2022-10-26 23:53:55 +0400

    .

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,5 +3,6 @@ const router = new Router();
 const adminController = require('../Controllers/adminController');
 
 router.post('/', adminController.check);
+router.put('/', adminController.create);
 
 module.exports = router;
